02326c3db03271f913e944b0508f09e3d6284e31
Satyam Sagar <satyamsagar1711@gmail.com>
2022-12-06 19:45:35 +0530

    feat: frames

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('output').src = localStorage.getItem('image') || '';
+    document.getElementById('download').href = localStorage.getItem('image') || '';
+    localStorage.getItem('image') ? document.getElementById('download').style.display = 'block' : document.getElementById('download').style.display = 'none';
+    
+
     document.getElementById('input').onchange = (e) => {
         document.getElementById('cropmodalbutton').click();
 
@@ -52,33 +57,26 @@ document.addEventListener('DOMContentLoaded', () => {
                 context1.rotate(-radians);
                 context1.translate(-canvas1.width/2,-canvas1.height/2);
             }
-            let flippedh = false, flippedv = false;
 
             document.getElementById('fliph').onclick = () => {
-                context1.clearRect(0, 0, canvas1.width, canvas1.height)
-                
-                context1.save();
-                context1.scale(flippedh ? 1: -1, 1);
-                context1.drawImage(initialimage, flippedh ? 0: canvas1.width * -1 , 0, canvas1.width, canvas1.height);
-                context1.restore();
-                if (flippedh){
-                    flippedh = false;
-                } else {
-                    flippedh = true;
-                }
+                let c = document.createElement('canvas');
+                c.width = canvas1.width;
+                c.height = canvas1.height;
+                let ctx = c.getContext('2d');
+                ctx.translate(canvas1.width, 0);
+                ctx.scale(-1, 1);
+                ctx.drawImage(canvas1, 0, 0, canvas1.width, canvas1.height);
+                context1.drawImage(c, 0, 0, canvas1.width, canvas1.height);
             }
             document.getElementById('flipv').onclick = () => {
-                context1.clearRect(0, 0, canvas1.width, canvas1.height)
-                
-                context1.save();
-                context1.scale(1, flippedv ? 1: -1);
-                context1.drawImage(initialimage, 0 , flippedv ? 0: canvas1.height * -1, canvas1.width, canvas1.height);
-                context1.restore();
-                if (flippedv){
-                    flippedv = false;
-                } else {
-                    flippedv = true;
-                }
+                let c = document.createElement('canvas');
+                c.width = canvas1.width;
+                c.height = canvas1.height;
+                let ctx = c.getContext('2d');
+                ctx.translate(0, canvas1.height);
+                ctx.scale(1, -1);
+                ctx.drawImage(canvas1, 0, 0, canvas1.width, canvas1.height);
+                context1.drawImage(c, 0, 0, canvas1.width, canvas1.height);
             }
         }
 
@@ -93,8 +91,33 @@ document.addEventListener('DOMContentLoaded', () => {
             canvas2.height = parseInt(cropdiv.style.height) + 6;
             context2.drawImage(canvas1, parseInt(cropdiv.style.left) - parseInt(getComputedStyle(document.querySelector('#cropmodal .modal-body')).paddingLeft), parseInt(cropdiv.style.top) - parseInt(getComputedStyle(document.querySelector('#cropmodal .modal-body')).paddingTop), parseInt(cropdiv.style.width) + 6, parseInt(cropdiv.style.height) + 6, 0, 0, parseInt(cropdiv.style.width) + 6, parseInt(cropdiv.style.height) + 6);
 
+            document.querySelectorAll('.f').forEach((f) => {
+                f.onclick = () => {
+                    context2.clearRect(0, 0, canvas2.width, canvas2.height);
+                    let w = canvas2.width;
+                    let h = canvas2.height;
+                    if (w > h){
+                        canvas2.height = w;
+                    } else {
+                        canvas2.width = h;
+                    }
+                    context2.drawImage(canvas1, parseInt(cropdiv.style.left) - parseInt(getComputedStyle(document.querySelector('#cropmodal .modal-body')).paddingLeft), parseInt(cropdiv.style.top) - parseInt(getComputedStyle(document.querySelector('#cropmodal .modal-body')).paddingTop), parseInt(cropdiv.style.width) + 6, parseInt(cropdiv.style.height) + 6, 0, 0, canvas2.width, canvas2.height);
+                    context2.globalCompositeOperation = 'destination-in';
+                    context2.drawImage(document.querySelector("#" + f.id + " img"), 0, 0, canvas2.width, canvas2.width);
+                }
+            });
+            document.getElementById('original').onclick = () => {
+                canvas2.width = parseInt(cropdiv.style.width) + 6;
+                canvas2.height = parseInt(cropdiv.style.height) + 6;
+                context2.drawImage(canvas1, parseInt(cropdiv.style.left) - parseInt(getComputedStyle(document.querySelector('#cropmodal .modal-body')).paddingLeft), parseInt(cropdiv.style.top) - parseInt(getComputedStyle(document.querySelector('#cropmodal .modal-body')).paddingTop), parseInt(cropdiv.style.width) + 6, parseInt(cropdiv.style.height) + 6, 0, 0, parseInt(cropdiv.style.width) + 6, parseInt(cropdiv.style.height) + 6);    
+            }
+
             document.getElementById('final').onclick = () => {
                 document.getElementById('output').src = canvas2.toDataURL();
+                document.getElementById('output').style.display = 'block';
+                document.getElementById('download').style.display = 'block';
+                document.getElementById('download').href = canvas2.toDataURL();
+                localStorage.setItem('image', canvas2.toDataURL());
                 document.getElementById('close2').click();
             }
             document.getElementById('close1').click();
